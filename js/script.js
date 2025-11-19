aea9071aa2274a9866c6f024b3eb9e7bef02ae47
Gustavo <gustaaavox@gmail.com>
2025-11-19 20:42:39 -0300

    feat:added event listener to every n inside numeros array

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 const numeros = document.querySelectorAll(".tecla.numero");
 
-
+numeros.forEach((n) => {
+  n.addEventListener("click", () => {
+    console.log(n.dataset.valor);
+  });
+});
 
 console.log([...numeros]);
