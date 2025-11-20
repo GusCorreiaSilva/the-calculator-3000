bbae3d3d5c6541a011d589ec6761d1090245f32e
Gustavo <gustaaavox@gmail.com>
2025-11-19 21:54:07 -0300

    feat: added n value to the valor inner html

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 const numeros = document.querySelectorAll(".tecla.numero");
+const valor = document.querySelector(".valor")
 
 numeros.forEach((n) => {
   n.addEventListener("click", () => {
+    valor.innerHTML += Number(n.dataset.valor)
     console.log(n.dataset.valor);
   });
 });
