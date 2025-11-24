a734ecc50805c9ec4df5a03d87eb5fd7243741d4
Gustavo <gustaaavox@gmail.com>
2025-11-24 16:36:25 -0300

    docs: added commented info about the code

--- js/script.js
+++ js/script.js
@@ -2,12 +2,17 @@
 let a = null;
 let b = null;
 
+//Teclas numericas
 const numeros = document.querySelectorAll(".tecla.numero");
+
+//seleção do campo onde os valores vão ser mostrado 
 const valor = document.querySelector(".valor");
 valor.dataset.valor = 0;
 
+//add evento de click em cada tecla 
 [...numeros].forEach((n) => {
   n.addEventListener("click", () => {
+    //add valores a variaveis A e B
     if (a === null) {
       a = n.dataset.valor;
     } else {
